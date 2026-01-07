6eddc63a4c708da0ad248f1b467159eeee8a22e2
SnO2WMaN <me@sno2wman.net>
2026-01-07 19:15:19 +0900

    Monthly Reports for December 2025

--- Catalogue/Monthly/Main.lean
+++ Catalogue/Monthly/Main.lean
@@ -2,6 +2,7 @@ import Catalogue.Init
 
 import Catalogue.Monthly.«2025_10».Main
 import Catalogue.Monthly.«2025_11».Main
+import Catalogue.Monthly.«2025_12».Main
 
 open Verso.Genre
 open Verso.Genre.Manual
@@ -16,3 +17,4 @@ These are monthly reports of our formalized results in FFL.
 
 {include 0 Catalogue.Monthly.«2025_10».Main}
 {include 0 Catalogue.Monthly.«2025_11».Main}
+{include 0 Catalogue.Monthly.«2025_12».Main}
