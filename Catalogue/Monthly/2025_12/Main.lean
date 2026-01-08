99b8fdb9cfa15fdf98afce9c8d37d4d33489f42b
SnO₂WMaN <me@sno2wman.net>
2026-01-09 08:11:45 +0900

    add: Monthly Reports for December 2025 (#1)

---
+++ Catalogue/Monthly/2025_12/Main.lean
@@ -0,0 +1,37 @@
+import Catalogue.Init
+
+open Verso.Genre
+open Verso.Genre.Manual
+open Verso.Genre.Manual.InlineLean
+
+open LO
+
+set_option verso.docstring.allowMissing true
+
+
+#doc (Manual) "Monthly Report 2025/12" =>
+%%%
+tag  := "monthly-report-2025-12"
+%%%
+
+This page is the monthly report summarizing commits [between 2025/12/01 and 2025/12/31](https://github.com/FormalizedFormalLogic/Foundation/commits/master/?since=2025-12-01&until=2025-12-31).
+This month has primarily focused on refactoring, there's no major results.
+
+# Commits
+
+- `7639aa03`: add(FirstOrder/Kripke): constant domain ([#673](https://github.com/FormalizedFormalLogic/Foundation/pull/673))
+- `751f8b2c`: refactor: Rename `HRel` to `Rel` ([#672](https://github.com/FormalizedFormalLogic/Foundation/pull/672))
+- `f10384e5`: refactor(Modal): Remove duplicate theorem `iff_boxdot_GL_Grz` ([#671](https://github.com/FormalizedFormalLogic/Foundation/pull/671))
+- `4af02f03`: refactor(Propositional): Refactor ([#670](https://github.com/FormalizedFormalLogic/Foundation/pull/670))
+- `c6777126`: chore(deps): bump actions/cache from 4 to 5 ([#669](https://github.com/FormalizedFormalLogic/Foundation/pull/669))
+- `c0fbe733`: add(Propositional, Modal): `NP` is also modal companion of `VF` ([#668](https://github.com/FormalizedFormalLogic/Foundation/pull/668))
+- `70499203`: refactor(Modal/PLoN): Refactor ([#667](https://github.com/FormalizedFormalLogic/Foundation/pull/667))
+- `f9c7f562`: add(Propositional/FMT): Add `VF_Ser` ([#666](https://github.com/FormalizedFormalLogic/Foundation/pull/666))
+- `eda77489`: docs: Monthly Reports 2025/11 ([#654](https://github.com/FormalizedFormalLogic/Foundation/pull/654))
+- `4042db29`: add(Propositional, Modal): `N ⊢ φᵍʷ` then `VF ⊢ φ` ([#661](https://github.com/FormalizedFormalLogic/Foundation/pull/661))
+- `641a6616`: add(Propositional, Modal): `VF ⊢ φ` then `N ⊢ φᵍʷ` ([#660](https://github.com/FormalizedFormalLogic/Foundation/pull/660))
+- `403e7473`: add(Propositional): Add very weak Corsi's Logic `VF` ([#659](https://github.com/FormalizedFormalLogic/Foundation/pull/659))
+- `c7d69332`: ci: Cleanup cache when PR is closed ([#657](https://github.com/FormalizedFormalLogic/Foundation/pull/657))
+- `a06253a3`: add(Propositional): `WF` proves `collectOrAnd` ([#658](https://github.com/FormalizedFormalLogic/Foundation/pull/658))
+- `65f121f0`: add(Propositioanl): Add weak Corsi's logic `WF` ([#655](https://github.com/FormalizedFormalLogic/Foundation/pull/655))
+- `738e8212`: chore(deps): bump actions/checkout from 5 to 6 ([#656](https://github.com/FormalizedFormalLogic/Foundation/pull/656))
